4bf89ede8df2209e51259840837e481c071f7b06
Sebastian <sebastian.slc9@gmail.com>
2020-05-17 22:31:33 -0500

    Added hash function handler

---
+++ src/main/java/hashing/HashFunctionsHandler.java
@@ -0,0 +1,29 @@
+package hashing;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class HashFunctionsHandler {
+
+    private final int numFunctions;
+
+    public HashFunctionsHandler (int numFunctions) {
+        this.numFunctions = numFunctions;
+    }
+
+    public List<Function<String, Integer>> getFunctions (int length) {
+        return getIndependentSeeds().map(
+                seed ->
+                    (Function<String, Integer>) s -> MurmurHash3.murmurhash3_x86_32(s, 0, s.length(), seed)
+        ).collect(Collectors.toList());
+    }
+
+    private Stream<Integer> getIndependentSeeds () {
+        Random r = new Random();
+        return IntStream.range(1, numFunctions).map(x -> r.nextInt(Short.MAX_VALUE) + x).boxed();
+    }
+}
