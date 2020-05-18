b739879c556ca870e8101acf509aede7a5a71669
Sebastian <sebastian.slc9@gmail.com>
2020-05-17 23:20:21 -0500

    Added hash functions generator in BloomFilter

--- src/main/java/dataStructures/BloomFilter.java
+++ src/main/java/dataStructures/BloomFilter.java
@@ -1,18 +1,41 @@
 package dataStructures;
 
+import hashing.HashFunctionsHandler;
+
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BloomFilter <A> {
 
-    private List<Function<A, Integer>> hashFunctions;
-    private BitArray bitArray;
-    private int size;
+    private final List<Function<A, Integer>> hashFunctions;
+    private final BitArray bitArray;
+    private final int size;
 
-    public BloomFilter (int size) {
+    public BloomFilter (int size, int expectedNumEntries) {
         this.size = size;
         this.bitArray = new BitArray(size);
+        this.hashFunctions = generateFunctions(expectedNumEntries);
+    }
+
+    public BloomFilter (int size, List<Function<A, Integer>> hashFunctions) {
+        this.size = size;
+        this.bitArray = new BitArray(size);
+        this.hashFunctions = hashFunctions;
+    }
+
+    private List<Function<A, Integer>> generateFunctions (int expectedNumEntries) {
+        int numFunctions = getNumberOfFunctions(expectedNumEntries);
+        HashFunctionsHandler functionsHandler = new HashFunctionsHandler(numFunctions);
+        return functionsHandler
+                .getFunctions().stream()
+                .map(h -> (Function<A, Integer>) a -> h.apply(a.toString()))
+                .collect(Collectors.toList());
+    }
+
+    private int getNumberOfFunctions (int expectedNumEntries){
+        return (int) (size/expectedNumEntries*Math.log(2));
     }
 
     private Stream<Integer> getIndices (A a) {
